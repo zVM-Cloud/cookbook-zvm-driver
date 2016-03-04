99bf172e2d68f8aee2f951dc28e39f80c595e91e
zshuo <shuozhang@cn.ibm.com>
2016-03-04 15:09:25 +0800

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,29 @@
 # encoding: UTF-8
 #
-# Cookbook Name:: ibm-openstack-zvm-driver
+# Cookbook Name:: openstack-zvm-driver
 # Recipe:: default
 #
-# =================================================================
-# Licensed Materials - Property of IBM
+# Copyright 2014, 2015 IBM Corp.
 #
-# (c) Copyright IBM Corp. 2014, 2015 All Rights Reserved
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# US Government Users Restricted Rights - Use, duplication or
-# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-# =================================================================
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 require 'chefspec'
 
 require 'chef/application'
 require 'securerandom'
 
-ChefSpec::Coverage.start! { add_filter 'ibm-openstack-zvm-driver' }
+ChefSpec::Coverage.start! { add_filter 'openstack-zvm-driver' }
 
 ::LOG_LEVEL = :fatal
 ::REDHAT_OPTS = {
