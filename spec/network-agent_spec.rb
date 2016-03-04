5b2e48f28b4a3cc6b3c138f537b91966948072e7
zshuo <shuozhang@cn.ibm.com>
2016-03-04 15:08:15 +0800

    Update network-agent_spec.rb

--- spec/network-agent_spec.rb
+++ spec/network-agent_spec.rb
@@ -1,20 +1,26 @@
 # encoding: UTF-8
 #
-# Cookbook Name:: ibm-openstack-zvm-driver
+# Cookbook Name:: openstack-zvm-driver
 # Recipe:: network-agent
 #
-# =================================================================
-# Licensed Materials - Property of IBM
+# Copyright 2014, 2015 IBM Corp.
 #
-# (c) Copyright IBM Corp. 2014, 2015 All Rights Reserved
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# US Government Users Restricted Rights - Use, duplication or
-# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-# =================================================================
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 require_relative 'spec_helper'
 
-describe 'ibm-openstack-zvm-driver::network-agent' do
+describe 'openstack-zvm-driver::network-agent' do
   describe 'redhat' do
     include_context 'network-common-stubs'
     include_context 'zvm_driver_network_stubs'
